3741d0300608fc645977b7059fb1da3dbbf9f05a
Harry Peaker <HarryPeaker@Aridhia.net>
2017-05-02 17:17:40 +0100

    align code style in ui

--- workspace/mini-apps/table_statistics/ui.R
+++ workspace/mini-apps/table_statistics/ui.R
@@ -1,24 +1,24 @@
 
+
 source("global.R")
 
 tables <- xap.list_tables()
 
-ui <- fluidPage(theme = "theme.css",
-                includeScript("tablesorter.js"),
-                includeCSS("ts_styles.css"),
-                
-                fluidRow(
-                  column(6,
-                    xap.chooseDataTableUI("choose_data", label = NULL)
-                  ),
-                  column(6,
-                         actionButton("link", "View Data")
-                  )
-                ),
-                
-                bsModal("modal", "Plot", "link", size = "large", dataTableOutput("dt")),
-                
-                uiOutput("ui"),
-                uiOutput("modals"),
-                uiOutput("plot_modals")
+ui <- fluidPage(
+  theme = "theme.css",
+  includeScript("tablesorter.js"),
+  includeCSS("ts_styles.css"),
+  
+  fluidRow(column(
+    6,
+    xap.chooseDataTableUI("choose_data", label = NULL)
+  ),
+  column(6,
+         actionButton("link", "View Data"))),
+  
+  bsModal("modal", "Plot", "link", size = "large", dataTableOutput("dt")),
+  
+  uiOutput("ui"),
+  uiOutput("modals"),
+  uiOutput("plot_modals")
 )
