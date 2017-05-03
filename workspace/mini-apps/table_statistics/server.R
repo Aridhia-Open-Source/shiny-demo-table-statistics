3ae4d8c72875902e9b5016abfb99aeaf35b2e849
Harry Peaker <HarryPeaker@Aridhia.net>
2017-05-03 11:05:26 +0100

    simplify session$onFlushed function

--- workspace/mini-apps/table_statistics/server.R
+++ workspace/mini-apps/table_statistics/server.R
@@ -10,14 +10,17 @@ server <- function(input, output, session) {
   
   sessionVars <- reactiveValues(prev_data = "None", i = 0)
   
+  # create a summary of the selected data
   summ <- reactive(withProgress(message = "Calculating summary", value = 0,{
     my_summ(d())
   }))
   
+  # column names of the table
   n <- reactive({
-    print(names(d()))
     names(d())
   })
+  
+  # random ids to assign to created plots
   plot_ids <- reactive({
     paste0("plot", sample(1:100000), length(summ()))
   })
@@ -43,38 +46,23 @@ server <- function(input, output, session) {
   })
   
   # we need to call tablesorter on the table each time it is created
-  # needs an extra run-through on initialization
-  flushed <- 0
+  # each time reactives are flushed, check whether the selected table has changed and run apply js scripts if so
   session$onFlushed(function() {
-    isolate({
-      print(paste("Input", d_name()))
-      print(paste("prev", sessionVars$prev_data))
-    })
-    flushed <<- flushed + 1
-    print("Flushing Reactives")
     isolate(
       if(sessionVars$prev_data != d_name()) {
-        print("Table has changed. Applying tablesorter")
         session$sendCustomMessage(type = 'tablesorter', message = list(id = "myTable"))
         session$sendCustomMessage(type = 'row_click', message = list(id = "myTable"))
         sessionVars$prev_data <- d_name()
       }
     )
-    if(flushed < 3) {
-      print("Still Initializing")
-      session$sendCustomMessage(type = 'tablesorter', message = list(id = "myTable"))
-      session$sendCustomMessage(type = 'row_click', message = list(id = "myTable"))
-    }
   }, FALSE)
   
   
   # create a ui chunk containing the table
   output$t <- renderUI({
     withProgress(message = "Calculating table statistics", value = 0, {
-      print("Rendering Table...")
-      out <- create_table(summ(), n(), plot_ids())
-      print("Done")
-      out
+      # create the table
+      create_table(summ(), n(), plot_ids())
     })
   })
   
@@ -84,7 +72,6 @@ server <- function(input, output, session) {
       return(NULL)
     }
     ids <- paste0(plot_ids(), "modal")
-    print("Creating Plot Modals")
     lapply(1:length(nam), function(i) {
       create_plot_modal(nam[i], ids[i])
     })
@@ -101,7 +88,6 @@ server <- function(input, output, session) {
       create_modal(s[[i]], nam[i])
     })
   })
-  
 }
 
 
