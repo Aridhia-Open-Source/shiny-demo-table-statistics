5336611b49d27347147cef45e48c4da7f188422d
James Bonner <james.bonner@aridhia.com>
2017-05-31 12:18:15 +0100

    updated theme css

--- workspace/mini-apps/table_statistics/documentation_ui.R
+++ workspace/mini-apps/table_statistics/documentation_ui.R
@@ -24,6 +24,7 @@ documentation_tab <- function() {
            br(),
            p("The video below gives an overview on how to use the app:"),
            HTML('<iframe width="100%" height="500" src="//www.youtube.com/embed/rCDZzf4ragg" frameborder="0"></iframe>'),
+           br(),
            p(strong("NB: This R Shiny app is provided unsupported and at user's risk. If you
                                are planning to use this app to inform your study, please review the
                     code and ensure you are comfortable with the calculations made.")
