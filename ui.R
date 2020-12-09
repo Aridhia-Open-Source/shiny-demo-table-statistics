fced108836d8d24fee621c2833d0c567216f7312
Harry Peaker <HarryPeaker@Aridhia.net>
2020-12-09 01:21:56 +0000

    linting

--- ui.R
+++ ui.R
@@ -2,34 +2,36 @@
 
 tables <- xap.list_tables()
 
-ui <- fluidPage(width = 12,
-                theme = "theme.css",
-                includeScript("www/tablesorter.js"),
-                includeCSS("www/ts_styles.css"),
-                tabsetPanel( 
-                  tabPanel("Application",
-                           fluidPage(
-                             
-                             singleton(
-                               tags$head(tags$script(src = "tablesorter.js"))
-                             ),
-                             singleton(
-                               tags$head(tags$script(src = "row_click.js"))
-                             ),
-                             
-                             tags$div(class = "well",
-                                      fluidRow(column(
-                                        6,
-                                        h3("Select your dataset"),
-                                        xap.chooseDataTableUI("choose_data", label = NULL),
-                                        actionButton("link", "Preview table")
-                                      ))),
-                             
-                             bsModal("modal", "Plot", "link", size = "large", dataTableOutput("dt")),
-                             
-                             uiOutput("t"),
-                             uiOutput("modals"),
-                             uiOutput("plot_modals")
-                           )),
-                  documentation_tab()
-                ))
+ui <- fluidPage(
+  width = 12,
+  theme = "theme.css",
+  includeScript("www/tablesorter.js"),
+  includeCSS("www/ts_styles.css"),
+  tabsetPanel( 
+    tabPanel("Application",
+      fluidPage(
+        singleton(
+          tags$head(tags$script(src = "tablesorter.js"))
+        ),
+        singleton(
+          tags$head(tags$script(src = "row_click.js"))
+        ),
+        tags$div(class = "well",
+          fluidRow(
+            column(6,
+              h3("Select your dataset"),
+              xap.chooseDataTableUI("choose_data", label = NULL),
+              actionButton("link", "Preview table")
+            )
+          )
+        ),
+        bsModal("modal", "Plot", "link", size = "large", dataTableOutput("dt")),
+        uiOutput("t"),
+        uiOutput("modals"),
+        uiOutput("plot_modals")
+      )
+    ),
+    documentation_tab()
+  )
+)
+
