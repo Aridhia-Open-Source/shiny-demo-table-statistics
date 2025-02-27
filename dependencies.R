e4488a7dcbcfb34d82e987bc11f9f6131375026f
Bert Overduin <bert.overduin@aridhia.com>
2025-02-27 09:13:57 +0000

    Added check to dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -1,1 +1,15 @@
-install.packages(c("shiny", "ggvis", "shinyBS"))
+package_install <- function(x, ...) {
+  for (i in x) {
+    # Check if package is installed
+    if (!require(i, character.only = TRUE)){
+      # If the package could not be loaded then install it
+      install.packages(i)
+    }
+  }
+}
+
+# Source this script to install all the libraries needed for the app
+
+packages <- c("shiny", "ggvis", "shinyBS")
+
+package_install(packages)
